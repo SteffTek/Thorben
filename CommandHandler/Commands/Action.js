d2bf67229a046e96456cd46b4bbb6443cc61a4d9
stefftek <spfahler@t-online.de>
2021-05-03 16:56:51 +0200

    Strings for Gifs

--- CommandHandler/Commands/Action.js
+++ CommandHandler/Commands/Action.js
@@ -29,6 +29,31 @@ const categories = [
     "yurikiss"
 ]
 
+const strings = {
+    "baka":"%n1% findet %n2% doof!",
+    "bite":"%n1% beißt %n2%!",
+    "bonk":"%n2% **BONK**!",
+    "highfive":"%n1% fordert %n2% zum Highfive auf!",
+    "hug":"%n1% umarmt %n2%.",
+    "kill":"%n1% bringt %n2% um!",
+    "kiss":"%n1% küsst %n2% :3",
+    "lick":"%n1% leckt %n2% ab.",
+    "love":"%n1% liebt %n2%! <3",
+    "marry":"%n1% will %n2% heiraten! So Cute :3",
+    "massage":"%n1% massiert %n2%.",
+    "pat":"%n1% streichelt %n2%! _PAT PAT_",
+    "poke":"%n1% stupst %n2% an.",
+    "punch":"%n1% schlägt %n2% richtig fest!",
+    "slap":"%n1% schlägt %n2%.",
+    "throw":"%n1% wirft etwas nach %n2%!",
+    "tickle":"%n1% kitzelt %n2%. Hihi ^^",
+    "wave":"%n1% winkt %n2% zu.",
+    "yaoihug":"%n1% umarmt %n2% :3",
+    "yaoikiss":"%n1% küsst %n2% ^^",
+    "yurihug":"%n1% umarmt %n2% :3",
+    "yurikiss":"%n1% küsst %n2% ^^"
+}
+
 class Meme extends Command {
 
     constructor(commandManager, name, description, syntax) {
@@ -46,7 +71,7 @@ class Meme extends Command {
         }
 
         if(mentioned.id === message.author.id) {
-            //return callback("Du kannst dich nicht selbst angeben!");
+            return callback("Du kannst dich nicht selbst angeben!");
         }
 
         //SETUP STRING
@@ -66,7 +91,7 @@ class Meme extends Command {
         let embed = new Discord.MessageEmbed()
             .setAuthor(message.author.tag, message.author.avatarURL())
             .setColor(this.commandManager.config.colors.embed)
-            .setDescription(`**${message.member.user}** >> **${mentioned.user}**`)
+            .setDescription(strings[action].replace("%n1%", `**${message.member.user}**`).replace("%n2%",`**${mentioned.user}**`))
             .setImage("https://images.stefftek.de" + gif.url);
 
         //SENDING MESSAGE
